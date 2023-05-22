68dc67a38af703f99ea17cd883211d40da93e239
solita-jajuhir <jaakko.hirvioja@solita.fi>
2023-05-22 10:17:57 +0300

    Fix: try out pm2 for the current configuration

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: "Lynx Web Shop App",
+      script: "./node_modules/next/dist/bin/next",
+      args: "start -p " + (process.env.PORT || 3000),
+      watch: false,
+      autorestart: true,
+    },
+  ],
+};

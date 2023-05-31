0c8f5806e51e3002e5f8d77c1d886fbdf76d1304
timole <timo.lehtonen@gmail.com>
2023-05-31 16:10:26 +0300

    refactor(main.tf): remove unused resource azurerm_linux_web_app. It is no longer needed as the app is now deployed using containers.

--- infra/main.tf
+++ infra/main.tf
@@ -26,29 +26,3 @@ variable "tags" {
 }
 
 data "azurerm_client_config" "current" {}
-
-resource "azurerm_resource_group" "lynx_web_shop_dev" {
-  name     = "${var.application_name}-dev-rg"
-  location = "North Europe"
-  tags = var.tags
-}
-
-resource "azurerm_service_plan" "lynx_web_shop_dev" {
-  name                = "lynx-web-shop-plan"
-  location            = azurerm_resource_group.lynx_web_shop_dev.location
-  resource_group_name = azurerm_resource_group.lynx_web_shop_dev.name
-  sku_name = "S1"
-  os_type = "Linux"
-
-  tags = var.tags
-}
-
-resource "azurerm_linux_web_app" "lynx_web_shop_dev" {
-  name                = "${var.application_name}-dev"
-  location            = azurerm_resource_group.lynx_web_shop_dev.location
-  resource_group_name = azurerm_resource_group.lynx_web_shop_dev.name
-  service_plan_id     = azurerm_service_plan.lynx_web_shop_dev.id
-
-  site_config {}
-  tags = var.tags
-}
